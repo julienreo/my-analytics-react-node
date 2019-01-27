e19edb0b1ccacc2853ac4793d03b30d2976559b5
julien <julienreo@gmail.com>
2019-01-27 14:46:29 +0100

    rename authenticate module

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const userController = require(__basedir + 'src/controllers/user');
-const authenticate = require(__basedir + 'src/middleware/authenticate');
+const auth = require(__basedir + 'src/middleware/auth');
 
 // Create new user
 router.post('/', userController.create);
@@ -10,6 +10,6 @@ router.post('/', userController.create);
 router.post('/login', userController.login);
 
 // Fetch user sites
-router.get('/me/sites', authenticate, userController.fetchSites);
+router.get('/me/sites', auth, userController.fetchSites);
 
 module.exports = router;
--- api/src/routes/visits.js
+++ api/src/routes/visits.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
 const visitController = require(__basedir + 'src/controllers/visit');
-const authenticate = require(__basedir + 'src/middleware/authenticate');
+const auth = require(__basedir + 'src/middleware/auth');
 
 // Create new visit (GET method is used as client request is made from an img tag)
 router.get('/create/:id', visitController.create);
 
 // Fetch visits
-router.get('/:id', authenticate, visitController.fetch);
+router.get('/:id', auth, visitController.fetch);
 
 module.exports = router;
