b2d258e9674058fd090ad2d81a3ff4ae9df5466c
julien <julienreo@gmail.com>
2019-01-28 00:14:41 +0100

    refactor visit controller

--- api/src/controllers/visit.js
+++ api/src/controllers/visit.js
@@ -11,17 +11,13 @@ const { validateVisit } = require(__basedir + 'src/models/Visit');
 const emptyGif = require(__basedir + 'lib/empty-gif');
 const asyncMiddleware = require(__basedir + 'src/middleware/async');
 
-
 /****************
  Create new visit
 *****************/ 
 
 exports.create = asyncMiddleware(async (req, res, next) => {
   
-  /* 
-   * Retrieve and process request data
-   */
-
+  // Retrieve and process request data
   const host = req.hostname;
   const ip = req.clientIp;
   const date = new Date();
@@ -31,10 +27,7 @@ exports.create = asyncMiddleware(async (req, res, next) => {
   const siteId = req.params.id;
 
 
-  /* 
-   * Format data
-   */
-
+  // Format data
   let isoCountryCode = geo === null || typeof geo === 'undefined' ? 'N/A' : geo.country;
   isoCountryCode = isoCountryCode === null || typeof isoCountryCode === 'undefined' ? 'N/A' : isoCountryCode;
 
@@ -47,40 +40,20 @@ exports.create = asyncMiddleware(async (req, res, next) => {
   const data = { host, ip, date, userAgent, isMobile, isoCountryCode, country, city, siteId };
 
 
-  /* 
-   * Validate data
-   */
-
+  // Validate data
   const { error } = validateVisit(data);
+  if (error) return res.status(400).send({ message: error.details[0].message });
   
-  if (error) {
-    const { message } = error.details[0];
-    return res.status(400).send({ message });
-  }
-  
-
-  /* 
-   * Save data
-   */
-
   // Check that site id is a valid ObjectId
-  const isSiteValid = mongoose.Types.ObjectId.isValid(data.siteId);
+  const isValid = mongoose.Types.ObjectId.isValid(data.siteId);
+  if (!isValid) return res.status(400).send({ message: 'Invalid site ID' });
 
-  // If site id is not valid
-  if (!isSiteValid) {
-    return res.status(400).send({ message: 'Invalid site ID' });
-  }
-
-  // Retrieve site from which visit comes from
+  // Retrieve site from which the visit comes from
   const site = await Site.findOne({ _id: mongoose.Types.ObjectId(data.siteId) });
-
-  // If site doesn't exit
-  if (site === null) {
-    return res.status(404).send({ message: 'Site not found' });
-  }
+  if (!site) return res.status(404).send({ message: 'Site not found' });
 
   // Save visit
-  const visitInstance = new Visit({
+  const newVisit = await new Visit({
     host: data.host,
     ip: data.ip,
     date: data.date,
@@ -91,18 +64,13 @@ exports.create = asyncMiddleware(async (req, res, next) => {
     city: data.city,
     site_id: site._id,
     user_id: site.user_id
-  });
+  }).save();
 
-  const newVisit = await visitInstance.save();
   log.info(`New visit: ${newVisit._id} registered`);
 
 
-  /* 
-   * Return empty Gif
-   */
-
-  res.set('Content-Type', 'image/gif');
-  res.send(emptyGif);
+  // Return empty Gif
+  res.set('Content-Type', 'image/gif').send(emptyGif);
 });
 
 
@@ -115,53 +83,31 @@ exports.fetch = asyncMiddleware(async (req, res, next) => {
   const siteId = req.params.id;
   const userId = req.token.id;
 
-  /* 
-   * Validate query params 
-   */
-
   // Check that site id is a valid ObjectId
   const isSiteValid = mongoose.Types.ObjectId.isValid(siteId);
-
-  // If site id is not valid
-  if (!isSiteValid) {
-    return res.status(400).send({ message: 'Invalid site ID' });
-  }
+  if (!isSiteValid) return res.status(400).send({ message: 'Invalid site ID' });
 
   // Check that site id exists
   const site = await Site.findOne({ _id: mongoose.Types.ObjectId(siteId) });
+  if (!site) return res.status(404).send({ message: 'Site not found' });
 
-  // If site doesn't exit
-  if (site === null) {
-    return res.status(404).send({ message: 'Site not found' });
-  }
-
-  // Check that site belongs to user
-  if (site.user_id !== userId) {
+  // Check that site belongs to the user
+  if (site.user_id !== userId)
     return res.status(403).send({ message: 'Forbidden access' });
-  }
 
   // If dates are missing
-  if (typeof from === 'undefined' || typeof to === 'undefined') {
+  if (!from || !to)
     return res.status(400).send({ message: 'Missing parameters' });
-  }
 
-
-  /* 
-   * Format data 
-   */
-
-  // Convert dates objects into strings using ISO standard
+  // Format dates
   from = moment(req.query.from).startOf('day'); 
   to = moment(req.query.to).endOf('day'); 
 
-
-  /* 
-   * Retrieve data 
-   */
-
+  // Retrieve visits
   const fetchedVisits = await Visit.find({
     date: { '$gte': from, '$lte': to },
     site_id: siteId
   });
+  
   return res.send(fetchedVisits);
 });
