6c9688f99d2682e403099ca283016a4705ab736d
julien <julienreo@gmail.com>
2019-01-27 23:53:53 +0100

    refactor user controller

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const mongoose = require('mongoose');
+const _ = require('lodash');
 
 const log = require(__basedir + 'lib/logger');
 const { User } = require(__basedir + 'src/models/User');
@@ -10,66 +11,38 @@ const { validateUserDetails } = require(__basedir + 'src/models/User');
 const Site = require(__basedir + 'src/models/Site');
 const asyncMiddleware = require(__basedir + 'src/middleware/async');
 
-
 /***************
  Create new user
 ****************/ 
 
 exports.create = asyncMiddleware(async (req, res, next) => {
-  const data = { username, email, password } = req.body;
+  const data = _.pick(req.body, ['username', 'email', 'password', 'sites']);
 
-  /* 
-   * Validate data
-   */
-  
+  // Validate data
   const { error } = validateUser(data);
+  if (error) return res.status(400).send({ message: error.details[0].message });
 
-  if (error) {
-    const { message } = error.details[0];
-    return res.status(400).send({ message });
-  }
-
-
-  /* 
-   * Hash password
-   */
-
+  // Hash password
   data.password = await bcrypt.hash(data.password, saltRounds);
 
-
-  /* 
-   * Save user in `users` collections and his sites in `sites` collection
-   */
-
   // Save user
-  const userInstance = new User(data);
-  const user = await userInstance.save();
+  const user = await new User(data).save();
   log.info(`New user: ${user.email} successfully saved`);
 
-
-  // Save user sites
+  // Save sites
   const newSitesIds = [];
   for (const site of user.sites) {
-    const siteInstance = new Site({
-      site: site,
-      user_id: user._id
-    });
+    const siteInstance = new Site({ site: site, user_id: user._id });
 
     const newSite = await siteInstance.save();
     log.info(`New site: ${newSite._id} successfully saved`);
 
-    // Push newly created sites ids into `newSitesIds` array
     newSitesIds.push(newSite._id);
   }
 
+  // Replace user sites names per their ids
+  await User.findOneAndUpdate({ _id: mongoose.Types.ObjectId(user._id) }, { sites: newSitesIds });
 
-  // In user document, replace user sites names per their respective ids
-  await User.findOneAndUpdate(
-    { _id: mongoose.Types.ObjectId(user._id) },
-    { sites: newSitesIds }
-  );
-
-  log.info(`New user: ${user.email} sites values replaced per their ids`);
   return res.send({ success: true });
 });
 
@@ -79,67 +52,29 @@ exports.create = asyncMiddleware(async (req, res, next) => {
 ************/ 
 
 exports.login = asyncMiddleware(async (req, res, next) => {
-  const data = { email, password } = req.body;
+  const data = _.pick(req.body, ['email', 'password']);
 
-  /* 
-   * Validate data
-   */
-  
+  // Validate data
   const { error } = validateUserDetails(data);
-  
-  if (error) {
-    const { message } = error.details[0];
-    return res.status(400).send({ message });
-  }
+  if (error) return res.status(400).send({ message: error.details[0].message });
 
-
-  /* 
-   *
-   * Find user and authenticate him
-   * 
-   */
-
+  // Find user
   const user = await User.findOne({ email: data.email });
+  if (!user) return res.status(404).send({ message: 'Utilisateur inconnu' });
 
-  // If user doesn't exist
-  if (user === null) {
-    log.error(`Failed to find user: ${data.email} when attempting to login`);
-    return res.status(404).send({ message: 'Utilisateur inconnu' });
-  }
-
-
-  /*
-    * Compare request and user passwords
-    */
-  
+  // Authenticate user
   const match = await bcrypt.compare(data.password, user.password);
+  if (!match) return res.status(403).send({ message: 'Mauvaise combinaison' });
 
-  // If passwords don't match
-  if (!match) {
-    log.error(`Failed to authenticate user: ${data.email}`);
-    return res.status(403).send({ message: 'Mauvaise combinaison' });
-  }
+  // Generate access token
+  const token = await jwt.sign(
+    { id: user._id, username: user.username, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: '2h' }
+  );
 
-
-  /*
-    * Generate token
-    */
-
-    const payload = {
-    id: user._id,
-    username: user.username,
-    email: user.email
-  };
-  
-  const token = await jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '2h' });
-  log.info(`User: ${user.email} has logged in`);
-
-  
-  /*
-    * Return token
-    */
-
-  return res.send({ message: 'Authentification réussie', token: token });
+  // Return token
+  return res.send({ message: 'Authentification réussie', token });
 });
 
 
@@ -148,8 +83,6 @@ exports.login = asyncMiddleware(async (req, res, next) => {
 *****************/ 
 
 exports.fetchSites = asyncMiddleware(async (req, res, next) => {
-  const userId = req.token.id;
-  
-  const sites = await Site.find({ user_id: userId });
+  const sites = await Site.find({ user_id: req.token.id });
   return res.send(sites);
 });
