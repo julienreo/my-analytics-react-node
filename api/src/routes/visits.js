cfc5c07a52dd8032b6cc21d34eaac3c3ddd6beba
julien <julienreo@gmail.com>
2019-01-27 14:50:42 +0100

    cleanup code

--- api/src/routes/visits.js
+++ api/src/routes/visits.js
@@ -3,10 +3,10 @@ const router = require('express').Router();
 const visitController = require(__basedir + 'src/controllers/visit');
 const auth = require(__basedir + 'src/middleware/auth');
 
-// Create new visit (GET method is used as client request is made from an img tag)
+// Create new visit for a specific site (GET method is used as client request is made from an img tag)
 router.get('/create/:id', visitController.create);
 
-// Fetch visits
+// Fetch visits for a specific site
 router.get('/:id', auth, visitController.fetch);
 
 module.exports = router;
